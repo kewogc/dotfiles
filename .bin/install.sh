b828066a845581107d58b636c2c77118ee068561
Lol kek <kewogc@gmail.com>
2024-11-20 20:49:58 +0700

    Update install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,4 +1,4 @@
-git clone --bare https://github.com/alexey-yanchenko/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/kewogc/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
